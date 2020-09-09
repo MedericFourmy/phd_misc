2d9bddbd4f6a35b7f568db6d57e3fb8b898e57a1
Médéric Fourmy <mfourmy@laas.fr>
2020-09-09 13:28:24 +0200

    Added axis angle orientation to mcapi plots

--- centroidalkin/mcapi_plots.py
+++ centroidalkin/mcapi_plots.py
@@ -49,21 +49,28 @@ errpz = df_est['pz'] - df_gtr['pz']
 # Extract quaternions to compute orientation difference (xyzw reversed in constructor -> normal)
 quat_gtr_lst = [pin.Quaternion(qw, qx, qy, qz) for qx, qy, qz, qw in zip(df_gtr['qx'], df_gtr['qy'], df_gtr['qz'], df_gtr['qw'])]
 quat_est_lst = [pin.Quaternion(qw, qx, qy, qz) for qx, qy, qz, qw in zip(df_est['qx'], df_est['qy'], df_est['qz'], df_est['qw'])]
+quat_kfs_lst = [pin.Quaternion(qw, qx, qy, qz) for qx, qy, qz, qw in zip(df_kfs['qx'], df_kfs['qy'], df_kfs['qz'], df_kfs['qw'])]
+
+# compute orientation in axis angle rep (w_phi_b)
+o_gtr_arr = np.array([pin.log3(q.toRotationMatrix()) for q in quat_gtr_lst])
+o_est_arr = np.array([pin.log3(q.toRotationMatrix()) for q in quat_est_lst])
+o_kfs_arr = np.array([pin.log3(q.toRotationMatrix()) for q in quat_kfs_lst])
+
 #  q_est - q_gtr =def log3(q_gtr.inv * q_est)
 quat_err_arr = np.array([pin.log3((q_gtr.inverse()*q_est).toRotationMatrix()) for q_gtr, q_est in zip(quat_gtr_lst, quat_est_lst)])
 quat_err_mag = np.apply_along_axis(np.linalg.norm, 1, quat_err_arr)
-errvx = df_est['vx'] - df_gtr['vx']
-errvy = df_est['vy'] - df_gtr['vy']
-errvz = df_est['vz'] - df_gtr['vz']
-errcx = df_est['cx'] - df_gtr['cx']
-errcy = df_est['cy'] - df_gtr['cy']
-errcz = df_est['cz'] - df_gtr['cz']
+errvx =  df_est['vx'] - df_gtr['vx']
+errvy =  df_est['vy'] - df_gtr['vy']
+errvz =  df_est['vz'] - df_gtr['vz']
+errcx =  df_est['cx'] - df_gtr['cx']
+errcy =  df_est['cy'] - df_gtr['cy']
+errcz =  df_est['cz'] - df_gtr['cz']
 errcdx = df_est['cdx'] - df_gtr['cdx']
 errcdy = df_est['cdy'] - df_gtr['cdy']
 errcdz = df_est['cdz'] - df_gtr['cdz']
-errLx = df_est['Lx'] - df_gtr['Lx']
-errLy = df_est['Ly'] - df_gtr['Ly']
-errLz = df_est['Lz'] - df_gtr['Lz']
+errLx =  df_est['Lx'] - df_gtr['Lx']
+errLy =  df_est['Ly'] - df_gtr['Ly']
+errLz =  df_est['Lz'] - df_gtr['Lz']
 
 def rmse(err_arr):
     return np.sqrt(np.mean(err_arr**2))
@@ -176,34 +183,54 @@ if ABSOLUTE_BASE:
     plt.title('Base position XYZ')
     plt.subplot(3,1,1)
     plt.plot(df_est['t'], df_est['px'], 'r--', label='px_est')
-    plt.plot(df_gtr['t'], df_gtr['px'], 'r', label='px_gtr')
-    plt.plot(df_kfs['t'], df_kfs['px'], 'rx', label='px_kf')
+    plt.plot(df_gtr['t'], df_gtr['px'], 'r',   label='px_gtr')
+    plt.plot(df_kfs['t'], df_kfs['px'], 'rx',  label='px_kf')
     plt.subplot(3,1,2)
     plt.plot(df_est['t'], df_est['py'], 'g--', label='py_est')
-    plt.plot(df_gtr['t'], df_gtr['py'], 'g', label='py_gtr')
-    plt.plot(df_kfs['t'], df_kfs['py'], 'gx', label='py_kf')
+    plt.plot(df_gtr['t'], df_gtr['py'], 'g',   label='py_gtr')
+    plt.plot(df_kfs['t'], df_kfs['py'], 'gx',  label='py_kf')
     plt.subplot(3,1,3)
     plt.plot(df_est['t'], df_est['pz'], 'b--', label='pz_est')
-    plt.plot(df_gtr['t'], df_gtr['pz'], 'b', label='pz_gtr')
-    plt.plot(df_kfs['t'], df_kfs['pz'], 'bx', label='pz_kf')
+    plt.plot(df_gtr['t'], df_gtr['pz'], 'b',   label='pz_gtr')
+    plt.plot(df_kfs['t'], df_kfs['pz'], 'bx',  label='pz_kf')
     plt.legend()
     if SAVE_FIGURES: fig.savefig(PATH+'base_pos.png')
+
+
+    # Base orientation
+    fig = plt.figure('Base orientation AngleAxis World frame')
+    plt.title('Base orientation XYZ')
+    plt.subplot(3,1,1)
+    plt.plot(df_est['t'], o_est_arr[:,0], 'r--', label='ox_est')
+    plt.plot(df_gtr['t'], o_gtr_arr[:,0], 'r',   label='ox_gtr')
+    plt.plot(df_kfs['t'], o_kfs_arr[:,0], 'rx',  label='ox_kf')
+    plt.subplot(3,1,2)
+    plt.plot(df_est['t'], o_est_arr[:,1], 'g--', label='oy_est')
+    plt.plot(df_gtr['t'], o_gtr_arr[:,1], 'g',   label='oy_gtr')
+    plt.plot(df_kfs['t'], o_kfs_arr[:,1], 'gx',  label='oy_kf')
+    plt.subplot(3,1,3)
+    plt.plot(df_est['t'], o_est_arr[:,2], 'b--', label='oz_est')
+    plt.plot(df_gtr['t'], o_gtr_arr[:,2], 'b',   label='oz_gtr')
+    plt.plot(df_kfs['t'], o_kfs_arr[:,2], 'bx',  label='oz_kf')
+    plt.legend()
+    if SAVE_FIGURES: fig.savefig(PATH+'base_angleaxis.png')
+
 
     # Base velocity
     fig = plt.figure('Base velocity XYZ')
     plt.title('Base velocity XYZ')
     plt.subplot(3,1,1)
     plt.plot(df_est['t'], df_est['vx'], 'r--', label='vx_est')
-    plt.plot(df_gtr['t'], df_gtr['vx'], 'r', label='vx_gtr')
-    plt.plot(df_kfs['t'], df_kfs['vx'], 'rx', label='vx_kf')
+    plt.plot(df_gtr['t'], df_gtr['vx'], 'r',   label='vx_gtr')
+    plt.plot(df_kfs['t'], df_kfs['vx'], 'rx',  label='vx_kf')
     plt.subplot(3,1,2)
-    plt.plot(df_est['t'], df_est['vy'], 'g--', label='cy_est')
-    plt.plot(df_gtr['t'], df_gtr['vy'], 'g', label='cy_gtr')
-    plt.plot(df_kfs['t'], df_kfs['vy'], 'gx', label='cy_kf')
+    plt.plot(df_est['t'], df_est['vy'], 'g--', label='vy_est')
+    plt.plot(df_gtr['t'], df_gtr['vy'], 'g',   label='vy_gtr')
+    plt.plot(df_kfs['t'], df_kfs['vy'], 'gx',  label='vy_kf')
     plt.subplot(3,1,3)
     plt.plot(df_est['t'], df_est['vz'], 'b--', label='vz_est')
-    plt.plot(df_gtr['t'], df_gtr['vz'], 'b', label='vz_gtr')
-    plt.plot(df_kfs['t'], df_kfs['vz'], 'bx', label='vz_kf')
+    plt.plot(df_gtr['t'], df_gtr['vz'], 'b',   label='vz_gtr')
+    plt.plot(df_kfs['t'], df_kfs['vz'], 'bx',  label='vz_kf')
     plt.legend()
     if SAVE_FIGURES: fig.savefig(PATH+'base_vel.png')
 
