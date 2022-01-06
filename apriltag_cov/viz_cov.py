3c47fa10021b2587436fc5ac7bd23c1c62ec66c1
Mederic Fourmy <mederic.fourmy@gmail.com>
2022-01-06 11:18:36 +0100

    refactoring viz_cov

--- apriltag_cov/viz_cov.py
+++ apriltag_cov/viz_cov.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg.linalg import inv
 import pinocchio as pin
 import matplotlib.pyplot as plt
 import plotly.offline as pltly
@@ -33,7 +34,7 @@ def project_multiple(T_lst, K, a_corners, sig_pix):
     return points_lst, Q_lst
 
 
-def get_ellipses(T_lst, Q_lst):
+def cov2ellipses(T_lst, Q_lst):
     ells = []
     for i in range(len(T_lst)):
         # API takes the information matrix, NOT the covariance
@@ -43,58 +44,75 @@ def get_ellipses(T_lst, Q_lst):
     return ells
 
 
+def plot_plotly(ells):
+    # plotly plot covariances
+    meshes = [ellipsoid_meshes(ell) for ell in ells]
+    pltly.plot(meshes)
 
 
-width, height, K = get_cam_model('camera_realsense.yml')
-tag_width = 0.2
-sig_pix = 3.0
-a_corners = 0.5*tag_width*np.array([
-    [-1,  1, 0], # bottom left
-    [ 1,  1, 0], # bottom right
-    [ 1, -1, 0], # top right
-    [-1, -1, 0], # top left
-])
+def plot_mpl(ells, ax):
+    plot_ellipsoid_3d_mpl(ells[0], ax)
+    plot_ellipsoid_3d_mpl(ells[1], ax)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    set_axes_equal(ax)
 
 
-t1 = np.array([0.15, 0, 2.0])
-R1 = pin.rpy.rpyToMatrix(np.deg2rad([0.0, 80, 0]))
-T1 = pin.SE3(R1, t1)
-
-t2 = np.array([-0.15, 0, 2.0])
-R2 = pin.rpy.rpyToMatrix(np.deg2rad([0.0, -80, 0]))
-T2 = pin.SE3(R2, t2)
-
-T_lst = [T1, T2]
-
-points_lst, Q_lst = project_multiple(T_lst, K, a_corners, sig_pix)
-Q_lst = [Q[:3,:3] for Q in Q_lst]
-ells = get_ellipses(T_lst, Q_lst)
+def plot_frames(T_lst, ax):
+    pass
 
 
-# plotly plot covariances
-meshes = [ellipsoid_meshes(ell) for ell in ells]
-pltly.plot(meshes)
+def plot_projs(points_lst, ax):
+    # plot projections
+    for points in points_lst:
+        plot_tag_projection(points)
+    plt.xlim(0, width)
+    plt.ylim(0, height)
+    fig.axes[0].set_aspect('equal')
 
 
-plt.rcParams["figure.figsize"] = (15,10) 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d') 
-plot_ellipsoid_3d_mpl(ells[0], ax)
-plot_ellipsoid_3d_mpl(ells[1], ax)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-set_axes_equal(ax)
-plt.show()
+
+if __name__ == '__main__':
+
+    width, height, K = get_cam_model('camera_realsense.yml')
+    tag_width = 0.2
+    sig_pix = 3.0
+    a_corners = 0.5*tag_width*np.array([
+        [-1,  1, 0], # bottom left
+        [ 1,  1, 0], # bottom right
+        [ 1, -1, 0], # top right
+        [-1, -1, 0], # top left
+    ])
 
 
-# plot projections
-fig = plt.figure()
-for points in points_lst:
-    plot_tag_projection(points)
-plt.xlim(0, width)
-plt.ylim(0, height)
-fig.axes[0].set_aspect('equal')
+    t1 = np.array([0.15, 0, 2.0])
+    R1 = pin.rpy.rpyToMatrix(np.deg2rad([0.0, 80, 0]))
+    T1 = pin.SE3(R1, t1)
+
+    t2 = np.array([-0.15, 0, 2.0])
+    R2 = pin.rpy.rpyToMatrix(np.deg2rad([0.0, -80, 0]))
+    T2 = pin.SE3(R2, t2)
+
+    T_lst = [T1, T2]
+
+    points_lst, Q_lst = project_multiple(T_lst, K, a_corners, sig_pix)
+    Q_lst = [Q[:3,:3] for Q in Q_lst]
+    ells = cov2ellipses(T_lst, Q_lst)
+
+    # PLOTLY
+    plot_plotly(ells)
+
+    plt.rcParams["figure.figsize"] = (15,10) 
+    # MATPLOTLIB
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d') 
+    plot_mpl(ells, ax)
+
+    # 2D projection of tags
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    plot_projs(points_lst, ax)
 
 
-# plt.show()
+    plt.show()
